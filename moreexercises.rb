a4f5bcd08db794b3a1aa5fc99fe5bda448836eb1
Chris Shieh <dev@chrisgrace.com>
2022-10-06 17:42:09 -0700

    exercsies

--- moreexercises.rb
+++ moreexercises.rb
@@ -1,3 +1,10 @@
-car = [[:type,'sedan'],[:color,'blue'],[:year,2003]]
+def string_lengths(sentence)
+  strings = sentence.split
+  lengths = []
 
-p car
+  strings.each do |string|
+    lengths << string.size
+  end
+end
+
+p string_lengths("To be or not to be")
